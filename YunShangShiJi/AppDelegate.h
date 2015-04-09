0be6da99a21306f195396e3169d975503f7051ae
ios-1 <ios-1@ios-1deMacBook-Air.local>
2015-04-09 08:55:51 +0800

    Initial Commit

---
+++ YunShangShiJi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YunShangShiJi
+//
+//  Created by ios-1 on 15/4/9.
+//  Copyright (c) 2015年 ios-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
